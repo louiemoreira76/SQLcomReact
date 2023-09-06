8be33b752240e3df72affc45d7521a276c6ca422
Luis <luis010101work@outlook.com>
2023-09-06 19:29:55 -0300

    Adicionando as paginas

---
+++ site/src/Routes.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/login'
+import Home from './pages/home'
+import Cadastrar from './pages/register'
+import Consultar from './pages/consult'
+import ConsultarCard from './pages/consultCard'
+import Detalhe from './pages/detalhe'
+
+
+export default function Routess(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Login />} />
+                <Route path='/admin' element={<Home />} />
+                <Route path='/admin/cadastrar' element={<Cadastrar />} />
+                <Route path='/admin/consultar' element={<Consultar />} />
+                <Route path='/admin/consultarCard' element={<ConsultarCard />} />
+                <Route path='/admin/detalhe' element={<Detalhe />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
